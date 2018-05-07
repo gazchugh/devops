5c1fd3f22cf8597f3d4cfead9e3b5b97de95e5f8
gazchugh <gazchugh@DIN55001813.corp.capgemini.com>
2018-05-07 14:30:13 +0530

    hiii

---
+++ Hello/src/gazal.java
@@ -0,0 +1,13 @@
+
+public class gazal {
+
+	public gazal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
